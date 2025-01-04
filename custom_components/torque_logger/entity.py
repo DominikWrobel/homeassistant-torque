924adc0116545255591d95794204d2e5858d05be
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2025-01-04 20:56:20 +0100

    Update entity.py
    - bug fixes

--- custom_components/torque_logger/entity.py
+++ custom_components/torque_logger/entity.py
@@ -12,15 +12,17 @@ if TYPE_CHECKING:
 class TorqueEntity(CoordinatorEntity):
     """Base Entity"""
     def __init__(self, coordinator: 'TorqueLoggerCoordinator',
-    config_entry: ConfigEntry, sensor_key: str, device: DeviceInfo):
+                config_entry: ConfigEntry, sensor_key: str, device: DeviceInfo):
         super().__init__(coordinator)
         self.config_entry = config_entry
         self.sensor_key = sensor_key
         self._car_id = list(device.get("identifiers"))[0][1]
         self._car_name = device.get("model")
         self._attr_device_info = device
+        # Include config entry ID in the unique_id
         self._attr_unique_id = f"{DOMAIN}_{config_entry.entry_id}_{self._car_id}_{sensor_key}"
         self._attr_attribution = ATTRIBUTION
         self._attr_extra_state_attributes = {
-            "car": self._car_name
+            "car": self._car_name,
+            "instance": config_entry.title  # Add the instance title to attributes
         }
