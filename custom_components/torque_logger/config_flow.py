744ee0adf72c8933c9503df9a034a7d48c095338
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2025-02-19 18:01:28 +0100

    Update config_flow.py
    v1.4 beta

--- custom_components/torque_logger/config_flow.py
+++ custom_components/torque_logger/config_flow.py
@@ -2,6 +2,7 @@
 from homeassistant import config_entries
 from homeassistant.core import callback
 import voluptuous as vol
+
 from .const import (
     CONF_EMAIL,
     CONF_IMPERIAL,
@@ -9,8 +10,10 @@ from .const import (
     PLATFORMS,
 )
 
+
 class TorqueLoggerFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for Torque Logger."""
+
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
@@ -22,35 +25,36 @@ class TorqueLoggerFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle a flow initialized by the user."""
         self._errors = {}
 
-        if user_input is not None:
-            # Check if an entry with this email already exists
-            await self.async_set_unique_id(user_input[CONF_EMAIL])
-            self._abort_if_unique_id_configured()
+        # Uncomment the next 2 lines if only a single instance of the integration is allowed:
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
 
+        if user_input is not None:
             valid = await self._test_credentials(
                 user_input[CONF_EMAIL]
             )
             if valid:
                 return self.async_create_entry(
-                    title=f"Torque Logger ({user_input[CONF_EMAIL]})",
-                    data=user_input
+                    title="Torque Logger", data=user_input
                 )
             else:
                 self._errors["base"] = CONF_EMAIL
+
             return await self._show_config_form(user_input)
 
         user_input = {}
         # Provide defaults for form
         user_input[CONF_EMAIL] = ""
+
         return await self._show_config_form(user_input)
 
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
         """Create the options flow."""
-        return TorqueLoggerOptionsFlowHandler(config_entry)
+        return TorqueLoggerOptionsFlowHandler()
 
-    async def _show_config_form(self, user_input):
+    async def _show_config_form(self, user_input: dict):  # pylint: disable=unused-argument
         """Show the configuration form to edit location data."""
         return self.async_show_form(
             step_id="user",
@@ -71,18 +75,14 @@ class TorqueLoggerFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 class TorqueLoggerOptionsFlowHandler(config_entries.OptionsFlow):
     """Torque Logger config flow options handler."""
 
-    def __init__(self, config_entry):
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
+    async def async_step_init(self, user_input=None):  # pylint: disable=unused-argument
         """Manage the options."""
         return await self.async_step_user()
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            return await self._update_options(user_input)
 
         return self.async_show_form(
             step_id="user",
@@ -94,4 +94,11 @@ class TorqueLoggerOptionsFlowHandler(config_entries.OptionsFlow):
                     for x in sorted(PLATFORMS)
                 }
             ),
+        )
+
+    async def _update_options(self, user_input):
+        """Update config entry options."""
+        return self.async_create_entry(
+            title=self.config_entry.data.get(CONF_EMAIL), 
+            data=user_input
         )
